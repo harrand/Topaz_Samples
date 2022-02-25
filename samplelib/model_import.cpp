4d29fe87d82b4f52d1fc67776a9b7df13c0fb8f9
Harrand <harrysjh98@gmail.com>
2022-02-25 01:43:42 +0000

    - Removed assimp and replaced with tinyobjloader because it sucked ass. 001_house now loads and displays... something... at least. should just load the other meshes and images and then work on matrix stuff and we'll be well-on-our-way :)

--- samplelib/model_import.cpp
+++ samplelib/model_import.cpp
@@ -1,15 +1,47 @@
 #include "model_import.hpp"
-#include "assimp/Importer.hpp"
-#include "assimp/scene.h"
-#include "assimp/postprocess.h"
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "tiny_obj_loader.h"
 
 namespace samplelib
 {
 	Mesh load_mesh(const char* relative_path)
 	{
-		Assimp::Importer importer;
-		const aiScene* scene = importer.ReadFile(relative_path, aiProcess_Triangulate | aiProcess_CalcTangentSpace | aiProcess_GenNormals);
-		tz_assert(scene != nullptr, "Model import failed: %s. Please submit a bug report.", importer.GetErrorString());
-		return {};
+		tinyobj::attrib_t attrib;
+		std::vector<tinyobj::shape_t> shapes;
+		std::vector<tinyobj::material_t> materials;
+		std::string warn, err;
+
+		auto res = tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, relative_path);
+		tz_assert(res, "Failed to load mesh from file \"%s\": %s", relative_path, (warn + err).c_str());
+		Mesh ret;
+		for(const tinyobj::shape_t& shape : shapes)
+		{
+			for(const tinyobj::index_t& index : shape.mesh.indices)
+			{
+				ret.vertices.add
+				({
+					.position =
+					{
+						attrib.vertices[3 * index.vertex_index + 0],
+						attrib.vertices[3 * index.vertex_index + 1],
+						attrib.vertices[3 * index.vertex_index + 2]
+					},
+					.texcoord =
+					{
+						attrib.texcoords[2 * index.texcoord_index + 0],
+						attrib.texcoords[2 * index.texcoord_index + 1]
+					},
+					.normal =
+					{
+						attrib.normals[3 * index.normal_index + 0],
+						attrib.normals[3 * index.normal_index + 1],
+						attrib.normals[3 * index.normal_index + 2]
+					},
+					.tangent = {0.0f, 0.0f, 0.0f},
+					.bitangent = {0.0f, 0.0f, 0.0f}
+				});
+			}
+		}
+		return ret;
 	}
 }

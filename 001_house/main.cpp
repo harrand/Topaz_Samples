f60946a8d5efb4a25cb44e0578e9d2dc38566ec7
Harrand <harrysjh98@gmail.com>
2022-02-27 03:47:05 +0000

    * Frame-independent-movement for 001_house

--- 001_house/main.cpp
+++ 001_house/main.cpp
@@ -2,6 +2,7 @@
 #include "core/peripherals/mouse.hpp"
 #include "core/tz.hpp"
 #include "core/matrix_transform.hpp"
+#include "core/time.hpp"
 #include "render.hpp"
 
 int main()
@@ -12,22 +13,25 @@ int main()
 		.name = "Sample 001 - House",
 	});
 	{
+		using namespace tz::literals;
 		game::RenderState state;
 		// Create renderer.
 		tz::gl2::Device dev;
 		tz::gl2::Renderer renderer = dev.create_renderer(state.info());
+		tz::Delay fixed_update{16670_us};
 		while(!tz::window().is_close_requested())
 		{
 			tz::window().update();
 			renderer.render(state.get_triangle_count());
-
+			// Fixed update of 60 per second (16.67ms == 16670us).
+			if(fixed_update)
 			{
 				auto is_key_down = [](tz::KeyCode code){return tz::window().get_keyboard_state().is_key_down(tz::peripherals::keyboard::get_key(code));};
 
 				game::GameRenderInfo& mutable_state = state.get_mutable_state(renderer);
 				tz::Vec4 cam_pos = {mutable_state.camera_pos[0], mutable_state.camera_pos[1], mutable_state.camera_pos[2], 0.0f};
 				tz::Vec3 cam_rot = mutable_state.camera_rot;
-				constexpr float multiplier = 0.2f;
+				constexpr float multiplier = 10.0f;
 				tz::Vec4 cam_forward = mutable_state.view_matrix * tz::Vec4{0.0f, 0.0f, -1.0f, 0.0f};
 				tz::Vec4 cam_right = mutable_state.view_matrix * tz::Vec4{-1.0f, 0.0f, 0.0f, 0.0f};
 				if(is_key_down(tz::KeyCode::W))
@@ -54,6 +58,10 @@ int main()
 				{
 					cam_pos[1] -= multiplier;
 				}
+				if(is_key_down(tz::KeyCode::Escape))
+				{
+					break;
+				}
 				static tz::Vec2i mouse_position;
 				tz::Vec2ui mp = tz::window().get_mouse_position_state().get_mouse_position();
 				tz::Vec2i mpi{static_cast<int>(mp[0]), static_cast<int>(mp[1])};
@@ -71,6 +79,7 @@ int main()
 				#if TZ_DEBUG
 					std::printf("Camera Pos: {%.2f, %.2f, %.2f} Rot: {%.2f, %.2f, %.2f}\r", cam_pos[0], cam_pos[1], cam_pos[2], cam_rot[0], cam_rot[1], cam_rot[2]);
 				#endif
+				fixed_update.reset();
 			}
 		}
 	}
